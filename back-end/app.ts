4a61eee6a1ec693c9ea9cf5319f5429a17687687
Jesse <sandvikjesse@gmail.com>
2023-01-02 02:57:33 -0500

    back-end: separated server from app for testing purposes

---
+++ back-end/app.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.json("Hello world!");
+});
+
+export default app;
