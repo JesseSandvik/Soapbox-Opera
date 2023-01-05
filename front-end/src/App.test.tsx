4acffd216736d87866786e15c8156c980d02497e
Jesse <sandvikjesse@gmail.com>
2023-01-04 20:39:21 -0800

    front-end: Incoming message display test written, failing, ready to write code to satisfy test

--- front-end/src/App.test.tsx
+++ front-end/src/App.test.tsx
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('Smoke Test', () => {
-  render(<App />);
-  const linkElement = screen.getByText( /hello world!/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  test("01. Incoming messages display in the conversation window", () => {
+    render(<App />);
+
+    const msgTextArea = screen.getByRole("textbox", { name: /message/i });
+
+    userEvent.click(msgTextArea);
+    userEvent.type(msgTextArea, "Hello Clarice.");
+
+    const sendMsgBtn = screen.getByRole("button", { name: /send/i });
+
+    userEvent.click(sendMsgBtn);
+
+    const conversationWindow = screen.getByText(/hello clarice./i);
+
+    expect(conversationWindow).toBeInTheDocument();
+    expect(conversationWindow).toHaveAttribute("id", "conversation-window");
+  });
 });
