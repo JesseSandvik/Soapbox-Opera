d4f8edebd9094008c78eda77d10e768c70c163e0
Jesse <sandvikjesse@gmail.com>
2023-01-04 21:31:55 -0800

    front-end: 01. Outbound messages display in the conversation window test passing, code written to satisfy test

--- front-end/src/App.test.tsx
+++ front-end/src/App.test.tsx
@@ -3,20 +3,20 @@ import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("<App />", () => {
-  test("01. Incoming messages display in the conversation window", () => {
+  test("01. Outbound messages display in the conversation window", () => {
     render(<App />);
 
-    const msgTextArea = screen.getByRole("textbox", { name: /message/i });
+    const msgTextArea = screen.getByRole("textbox");
 
     userEvent.click(msgTextArea);
-    userEvent.type(msgTextArea, "Hello Clarice.");
+    userEvent.type(msgTextArea, "Hello{space}Clarice.");
 
     const sendMsgBtn = screen.getByRole("button", { name: /send/i });
 
     userEvent.click(sendMsgBtn);
 
-    const conversationWindow = screen.getByText(/hello clarice./i);
-
+    const conversationWindow = screen.getByText("Hello Clarice.");
+    
     expect(conversationWindow).toBeInTheDocument();
     expect(conversationWindow).toHaveAttribute("id", "conversation-window");
   });
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [outboundMessage, setOutboundMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const handleSubmit = (event: React.FormEvent): void => {
+    event.preventDefault();
+    const updatedMessageList = [...messages, outboundMessage];
+    setOutboundMessage("");
+    setMessages(updatedMessageList);
+  }
+
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <div id="conversation-window">{messages}</div>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <textarea
+          name="outboundMessage"
+          onChange={(event) => setOutboundMessage(event.target.value)}
+          value={outboundMessage ? outboundMessage : ""}
+        />
+        <button type="submit">send</button>
+      </form>
     </div>
   );
 }
